a69fc0e020b1905d21be0ba21eed11d680856f31
Ayman Milhem <aymanmilhem@gmail.com>
2022-09-06 11:28:29 +1200

    add the carousel component

---
+++ src/Components/Carousel/Carousel.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import classes from '../Carousel/Carousel.module.css';
+
+
+function Carousel() {
+  return (
+    <div className={classes.carousel}>Carousel</div>
+  )
+}
+
+export default Carousel;
